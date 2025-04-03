26e363b175bee051c0a1b35f3d548a5081743b00
Priyank Keshri <priyankkeshri1268@gmail.com>
2025-04-04 00:45:24 +0530

    Update third.sh

--- bashScripts/third.sh
+++ bashScripts/third.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 jobDetail='the main job for this activity is to complete the job on time. eg. job is my job'
-echo "$jobDetail"
+echo "${jobDetail/,,}"
 
 kaam=${jobDetail/job/kaam}
-echo "$kaam"
+echo "${kaam^^}"
 
 echo "end now2"
