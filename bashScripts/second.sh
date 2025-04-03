21a86be654986ab658ae305df36771bc864586e0
priyankkeshri <priyankkeshri1268@gmail.com>
2025-04-03 18:33:57 +0000

    moving files

---
+++ bashScripts/second.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 'this is second shell script'
+
+if [[$1 -eq $2]]
+then
+	echo 'both are equal.'
+	
+fi
+
+if [[$1 -gt $2]]
+then
+	echo "$1 is greater than $2"
+fi
+
+if [[$1 -lt $2]]; then
+	echo "$1 is less than $2"
+fi
+
+if [[$1 -nt $2]]; then 
+echo "both are not equal"
+fi
+
+
